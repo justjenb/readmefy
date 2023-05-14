2730605eeb9cc9dc08a9d5416339363db7195d2a
Jen B <jenbutler984@gmail.com>
2023-05-14 10:26:45 -0400

    Added formatting for badge to change - to _

--- README.md
+++ README.md
@@ -19,25 +19,23 @@ undefined
 
 ## License
 
-![License](https://img.shields.io/badge/License-"AGPL-3.0"-blue.svg)
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
 
-**This project is licensed under the [AGPL-3.0](https://opensource.org/licenses/AGPL-3.0) license.**
-
-For more information about this license, please visit the following link:
-
-https://opensource.org/licenses/AGPL-3.0![License](https://img.shields.io/badge/License-"Apache-2.0"-blue.svg)
-
-**This project is licensed under the [Apache-2.0](https://opensource.org/licenses/Apache-2.0) license.**
+**This project is licensed under the [MIT](https://opensource.org/licenses/MIT) license.**
 
 For more information about this license, please visit the following link:
 
-https://opensource.org/licenses/Apache-2.0![License](https://img.shields.io/badge/License-"BSD-2-Clause"-blue.svg)
+https://opensource.org/licenses/MIT
 
-**This project is licensed under the [BSD-2-Clause](https://opensource.org/licenses/BSD-2-Clause) license.**
+![License](https://img.shields.io/badge/License-Unlicense-blue.svg)
+
+**This project is licensed under the [Unlicense](https://opensource.org/licenses/Unlicense) license.**
 
 For more information about this license, please visit the following link:
 
-https://opensource.org/licenses/BSD-2-Clause
+https://opensource.org/licenses/Unlicense
+
+
 
 ## Credits
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,35 +1,27 @@
 function renderLicenseBadge(license) {
-  console.log(`render license badge ${JSON.stringify(license)}`);
-  return `![License](https://img.shields.io/badge/License-${JSON.stringify(
-    license
-  )}-blue.svg)`;
+  const licensePart = license.replace(/-/g, "_");
+  return `![License](https://img.shields.io/badge/License-${licensePart}-blue.svg)`;
+
 }
 
 // TODO: Create a function that returns the license link
 function renderLicenseLink(license) {
-  console.log(`render license link ${JSON.stringify(license)}`);
   return `https://opensource.org/licenses/${license}`;
 }
 
 // TODO: Create a function that returns the license section of README
 function renderLicenseSection(data) {
-
   let licenseSection = "";
-
   for (const license of data.projLicense) {
     const licenseLink = renderLicenseLink(license);
     const licenseBadge = renderLicenseBadge(license);
-
-  
-    licenseSection += `${licenseBadge}\n\n**This project is licensed under the [${license}](${licenseLink}) license.**\n\nFor more information about this license, please visit the following link:\n\n${licenseLink}`;
+    licenseSection += `${licenseBadge}\n\n**This project is licensed under the [${license}](${licenseLink}) license.**\n\nFor more information about this license, please visit the following link:\n\n${licenseLink}\n\n`;
   }
   return licenseSection;
 }
 
 function generateMarkdown(data) {
-
   const selectedLicenses = renderLicenseSection(data);
- 
   const markdown = `
 # ${data.projectTitle}
 
